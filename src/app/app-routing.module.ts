baef5a1fd2155a4ab060a5fbd01fd2e741af4762
Helio Filho <helio.souza@fc.com.br>
2020-01-23 10:51:47 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from './../guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -11,45 +11,36 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: './login/login.module#LoginPageModule'
+    loadChildren: './page/login/login.module#LoginPageModule'
   },
   {
     path: 'pedido-lista',
-    loadChildren: './pedido-lista/pedido-lista.module#PedidoListaPageModule',
+    loadChildren: './page/pedido-lista/pedido-lista.module#PedidoListaPageModule',
     canActivate: [AuthGuard]
   },
   {
     path: 'pedido-resumo',
-    loadChildren: './pedido-lista/pedido-resumo/pedido-resumo.module#PedidoResumoPageModule'
+    loadChildren: './page/pedido-lista/pedido-resumo/pedido-resumo.module#PedidoResumoPageModule'
   },
   { 
     path: 'pedido-sacola', 
-    loadChildren: './pedido-sacola/pedido-sacola.module#PedidoSacolaPageModule' 
+    loadChildren: './page/pedido-sacola/pedido-sacola.module#PedidoSacolaPageModule' 
   },
   {
     path: 'produto',
-    loadChildren: './produto/produto.module#ProdutoPageModule'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
-    canActivate: [AuthGuard]
+    loadChildren: './page/produto/produto.module#ProdutoPageModule'
   },
   {
     path: 'pedido-rapido',
-    loadChildren: './pedido-rapido/pedido-rapido.module#PedidoRapidoPageModule'
-  },
-  {
-    path: 'to-pdf-page',
-    loadChildren: './to-pdf-page/to-pdf-page.module#ToPDFPagePageModule'
+    loadChildren: './page/pedido-rapido/pedido-rapido.module#PedidoRapidoPageModule'
   },
   {
     path: 'new-tms',
-    loadChildren: './new-tms/new-tms.module#NewTMSPageModule'
+    loadChildren: './page/new-tms/new-tms.module#NewTMSPageModule'
   },
   {
     path: 'indicador-vendedor',
-    loadChildren: './indicador-vendedor/indicador-vendedor.module#IndicadorVendedorPageModule'
+    loadChildren: './page/indicador-vendedor/indicador-vendedor.module#IndicadorVendedorPageModule'
   },
 
   { path: 'produto-imagens', loadChildren: './produto/produto-imagens/produto-imagens.module#ProdutoImagensPageModule' },
