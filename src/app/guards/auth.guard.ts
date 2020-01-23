56327ffd5b39308f7b48062d1f72a04b5f073202
Helio Filho <helio.souza@fc.com.br>
2020-01-23 14:09:43 -0300

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
+import { BaseCommon } from 'src/commons/base-common';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuard implements CanActivate {
 
   public logged: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public common: BaseCommon) { }
 
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    if (!this.logged) {
+      this.common.showAlertInfo("Login necessário!")
+      return this.router.navigateByUrl("/login")
+    }
+    return true
   }
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {
-
-    return this.logged
-
-  }
 }
