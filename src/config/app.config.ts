8b9b11ce14f712812b0d8bcbe81ae2c33d54f7ae
Helio Filho <helio.souza@fc.com.br>
2020-02-03 17:49:35 -0300

    Ajustes
    Tirando os importes desnecessarios

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { ENV } from '../environments/environment';
 
 export let API_URL: any;
