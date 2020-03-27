abcf33e7f60cd82104ae2d632f73becc856b93aa
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2020-03-27 11:48:55 -0300

    teste4-27/03/2020

--- src/app/page/formas-pagamento/parcelamento/parcelamento.page.ts
+++ src/app/page/formas-pagamento/parcelamento/parcelamento.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CondicaoPagamentoService } from 'src/app/services/pagamento/condicao-pagamento.service';
+import { PedidoService } from 'src/app/services/pedido/pedido.service';
+import { CommonService } from 'src/app/services/common/common.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-parcelamento',
@@ -7,7 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParcelamentoPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public common: CommonService,
+    public pedido: PedidoService,
+    private pagamento: CondicaoPagamentoService,
+    private navControl: NavController
+  ) { }
 
   ngOnInit() {
   }
