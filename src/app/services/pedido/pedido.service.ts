8e674e2e5c07c3d0ef1ea1e98f09b801fc801c71
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2020-05-30 16:16:18 -0300

    Update pedido.service.ts

--- src/app/services/pedido/pedido.service.ts
+++ src/app/services/pedido/pedido.service.ts
@@ -124,17 +124,14 @@ export class PedidoService {
       '/criar';
 
     return new Promise((resolve, reject) => {
-      this.baseService.post(link, {}).then(
-        (result: any) => {
-          this.atualizaPedidoHeader(result);
-          console.log('Pedido criado!');
-          resolve();
-        },
-        (error: any) => {
-          console.log(error);
-          reject();
-        }
-      );
+      this.baseService.post(link, {}).then((result: any) => {
+        this.atualizaPedidoHeader(result);
+        console.log('Pedido criado!');
+        resolve();
+      }, (error: any) => {
+        console.log(error);
+        reject();
+      });
     });
   }
 
@@ -182,15 +179,12 @@ export class PedidoService {
         this.numPedido;
 
       return new Promise((resolve, reject) => {
-        this.baseService.post(link, aResult).then(
-          () => {
-            resolve();
-          },
-          (error: any) => {
-            console.log(error);
-            reject();
-          }
-        );
+        this.baseService.post(link, aResult).then(() => {
+          resolve();
+        }, (error: any) => {
+          console.log(error);
+          reject();
+        });
       });
     }
   }
@@ -208,19 +202,16 @@ export class PedidoService {
       '/' +
       this.numPedido;
 
-    await this.baseService.post(link, aResult).then(
-      (result: any) => {
-        this.atualizaPedidoHeader(result);
-        this.cardSelected = true;
-        this.codigoCartaoPedido = codCard;
-        this.common.showToast('Cartão Pedido Adicionado!');
-      },
-      (error: any) => {
-        this.cardSelected = false;
-        this.codigoCartaoPedido = '';
-        console.log(error);
-      }
-    );
+    await this.baseService.post(link, aResult).then((result: any) => {
+      this.atualizaPedidoHeader(result);
+      this.cardSelected = true;
+      this.codigoCartaoPedido = codCard;
+      this.common.showToast('Cartão Pedido Adicionado!');
+    }, (error: any) => {
+      this.cardSelected = false;
+      this.codigoCartaoPedido = '';
+      console.log(error);
+    });
   }
 
   // by Hélio 11/02/2020
@@ -235,17 +226,14 @@ export class PedidoService {
       '/' +
       this.numPedido;
 
-    await this.baseService.post(link, aResult).then(
-      (result: any) => {
-        this.atualizaPedidoHeader(result);
-        this.clientSelected = true;
-        this.docCliente = cgccpf;
-        this.dadosCliente = dadosCli;
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
+    await this.baseService.post(link, aResult).then((result: any) => {
+      this.atualizaPedidoHeader(result);
+      this.clientSelected = true;
+      this.docCliente = cgccpf;
+      this.dadosCliente = dadosCli;
+    }, (error: any) => {
+      console.log(error);
+    });
   }
 
   // by Hélio - Retorna os dados do cliente selecionado
@@ -256,16 +244,13 @@ export class PedidoService {
       this.dadosCliente === undefined
     ) {
       await this.common.showLoader();
-      await this.clienteService.getClienteNoAlert(this.docCliente).then(
-        (result: any) => {
-          this.dadosCliente = result;
-          this.common.loading.dismiss();
-          return;
-        },
-        () => {
-          this.common.loading.dismiss();
-        }
-      );
+      await this.clienteService.getClienteNoAlert(this.docCliente).then((result: any) => {
+        this.dadosCliente = result;
+        this.common.loading.dismiss();
+        return;
+      }, () => {
+        this.common.loading.dismiss();
+      });
     } else {
       return;
     }
@@ -283,17 +268,14 @@ export class PedidoService {
       '/' +
       this.numPedido;
 
-    await this.baseService.post(link, aResult).then(
-      (result: any) => {
-        this.atualizaPedidoHeader(result);
-        this.clientSelected = false;
-        this.docCliente = '';
-        this.dadosCliente = undefined;
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
+    await this.baseService.post(link, aResult).then((result: any) => {
+      this.atualizaPedidoHeader(result);
+      this.clientSelected = false;
+      this.docCliente = '';
+      this.dadosCliente = undefined;
+    }, (error: any) => {
+      console.log(error);
+    });
   }
 
   // by Hélio 12/02/2020
@@ -338,15 +320,12 @@ export class PedidoService {
       '/' +
       pedidoId;
 
-    await this.baseService.post(link, {}).then(
-      () => {
-        this.limpaDadosPedido();
-        this.common.showToast('Pedido apagado!');
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
+    await this.baseService.post(link, {}).then(() => {
+      this.limpaDadosPedido();
+      this.common.showToast('Pedido apagado!');
+    }, (error: any) => {
+      console.log(error);
+    });
   }
 
   // abrindo pagina customizada utilizando parametros
