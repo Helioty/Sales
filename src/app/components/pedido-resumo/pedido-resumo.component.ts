4a6cc34f2d21c08444a3ad76dc33be395678bf3f
helio.souza <helio.souza@ferreiracosta.com.br>
2021-10-20 15:08:32 -0300

    Update pedido-resumo.component.ts

--- src/app/components/pedido-resumo/pedido-resumo.component.ts
+++ src/app/components/pedido-resumo/pedido-resumo.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { PedidoService } from 'src/app/services/pedido/pedido.service';
+import { PedidoHeader } from 'src/app/services/pedido/pedido.interface';
 
 @Component({
   selector: 'app-pedido-resumo',
@@ -6,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pedido-resumo.component.scss'],
 })
 export class PedidoResumoComponent implements OnInit {
-  constructor() {}
+  pedido: Observable<PedidoHeader>;
+  constructor(private readonly pedidoService: PedidoService) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.pedido = this.pedidoService.getPedidoAtivoOBS();
+  }
 }
