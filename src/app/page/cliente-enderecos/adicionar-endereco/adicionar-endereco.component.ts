254523d714f68ebc2a9263f80ccffa36a8f0bf99
helio.souza <helio.souza@ferreiracosta.com.br>
2021-10-27 18:32:33 -0300

    T

--- src/app/page/cliente-enderecos/adicionar-endereco/adicionar-endereco.component.ts
+++ src/app/page/cliente-enderecos/adicionar-endereco/adicionar-endereco.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { ClienteGet } from 'src/app/services/cliente/cliente.interface';
+import { ClienteGet, Endereco } from 'src/app/services/cliente/cliente.interface';
 import { ClienteService } from 'src/app/services/cliente/cliente.service';
 import { CommonService } from 'src/app/services/common/common.service';
 import { PedidoService } from 'src/app/services/pedido/pedido.service';
@@ -15,14 +16,27 @@ import { PedidoService } from 'src/app/services/pedido/pedido.service';
 export class AdicionarEnderecoComponent implements OnInit {
   @Input() cliente: ClienteGet;
 
+  form: FormGroup;
+
   constructor(
     private readonly modalCtrl: ModalController,
     private readonly pedidoService: PedidoService,
     private readonly clienteService: ClienteService,
+    private readonly formBuilder: FormBuilder,
     private readonly common: CommonService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      cidade: ['', Validators.required],
+      ds_ende: ['', Validators.required],
+      nu_ende: ['', Validators.required],
+      ds_bairro: ['', Validators.required],
+      ds_cep: ['', Validators.required],
+      ds_uf: ['', Validators.required],
+      ds_compl: [''],
+    });
+  }
 
   /**
    * @author helio.souza
@@ -38,9 +52,25 @@ export class AdicionarEnderecoComponent implements OnInit {
 
   /**
    * @author helio.souza
-   * @param data
+   * @param patch
    */
-  async cadastrar(data: any): Promise<void> {
+  patchForm(patch?: any): void {
+    this.form.patchValue({
+      cidade: patch ? '' : '',
+      ds_ende: '',
+      nu_ende: '',
+      ds_bairro: '',
+      ds_cep: '',
+      ds_uf: '',
+      ds_compl: '',
+    });
+  }
+
+  /**
+   * @author helio.souza
+   * @param data Dados do form de endereço
+   */
+  async cadastrar(data: Endereco): Promise<void> {
     await this.common.showLoaderCustom('Salvando...');
     const newCliente = { ...this.cliente, enderecos: [data, ...this.cliente.enderecos] };
     this.atualizaCadastroCliente(newCliente)
--- src/app/page/cliente-enderecos/cliente-enderecos.module.ts
+++ src/app/page/cliente-enderecos/cliente-enderecos.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { AdicionarEnderecoComponent } from './adicionar-endereco/adicionar-endereco.component';
@@ -13,6 +13,7 @@ import { ClienteEnderecosPage } from './cliente-enderecos.page';
     FormsModule,
     IonicModule,
     ComponentsModule,
+    ReactiveFormsModule,
     ClienteEnderecosPageRoutingModule,
   ],
   declarations: [ClienteEnderecosPage, AdicionarEnderecoComponent],
