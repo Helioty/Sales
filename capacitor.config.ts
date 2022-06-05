b09d67fd12dc18ef1900359ba25575cf34b8b3fb
Hélio Filho <39842088+Helioty@users.noreply.github.com>
2022-06-05 17:47:45 -0300

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,24 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frame',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      'android-minSdkVersion': '22',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000',
+      resizeOnFullScreen: 'true',
+      orientation: 'portrait'
+    }
+  }
+};
+
+export default config;
