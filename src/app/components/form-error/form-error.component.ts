69911db72deef76be8ae7239ecc2cceaa9c72c33
Hélio Souza <contatohelioseverino@gmail.com>
2023-01-17 00:50:21 -0300

    Update form-error.component.ts

--- src/app/components/form-error/form-error.component.ts
+++ src/app/components/form-error/form-error.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-form-error',
   templateUrl: './form-error.component.html',
   styleUrls: ['./form-error.component.scss'],
+  imports: [CommonModule],
 })
 export class FormErrorComponent {
   @Input() errorMessage: string;
