32e11064c6208571cc2e4dc98169f04313ca0719
Hélio Souza <contatohelioseverino@gmail.com>
2023-01-17 00:53:56 -0300

    Update pedido-resumo.component.ts

--- src/app/components/pedido-resumo/pedido-resumo.component.ts
+++ src/app/components/pedido-resumo/pedido-resumo.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { PedidoHeader } from 'src/app/services/pedido/pedido.interface';
 import { PedidoService } from 'src/app/services/pedido/pedido.service';
 
 @Component({
+  standalone: true,
   selector: 'app-pedido-resumo',
   templateUrl: './pedido-resumo.component.html',
   styleUrls: ['./pedido-resumo.component.scss'],
+  imports: [CommonModule, IonicModule],
 })
 export class PedidoResumoComponent implements OnInit {
   pedido: Observable<PedidoHeader>;
