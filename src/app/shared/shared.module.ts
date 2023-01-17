a16ab53cece92463c513e1394570a9ae16f835b1
Hélio Souza <contatohelioseverino@gmail.com>
2023-01-17 00:54:12 -0300

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { HideKeyboardDirective } from './custom/hide-keyboard.directive';
 
 @NgModule({
-  declarations: [HideKeyboardDirective],
   imports: [CommonModule],
-  exports: [HideKeyboardDirective],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
