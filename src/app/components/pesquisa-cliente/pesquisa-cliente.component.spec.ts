80766c0907213c0abca795411542f49cf3b610ea
Hélio Souza <contatohelioseverino@gmail.com>
2023-01-17 00:54:07 -0300

    Update pesquisa-cliente.component.spec.ts

--- src/app/components/pesquisa-cliente/pesquisa-cliente.component.spec.ts
+++ src/app/components/pesquisa-cliente/pesquisa-cliente.component.spec.ts
@@ -1,20 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { PesquisaClienteComponent } from './pesquisa-cliente.component';
 
 describe('PesquisaClienteComponent', () => {
   let component: PesquisaClienteComponent;
   let fixture: ComponentFixture<PesquisaClienteComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [PesquisaClienteComponent],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [PesquisaClienteComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PesquisaClienteComponent);
