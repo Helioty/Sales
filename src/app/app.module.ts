ae5d0cc16e823d0ebc59b19df4472ae9e0de225a
Hélio Souza <contatohelioseverino@gmail.com>
2023-01-17 00:50:12 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
-// import { AndroidFullScreen } from '@ionic-native/android-full-screen/ngx';
-// import { AppVersion } from '@ionic-native/app-version/ngx';
-// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-// import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { AppConfigService } from 'src/app/config/app.config.service';
 import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
-import { ServicesModule } from './services/services.module';
-import { SharedModule } from './shared/shared.module';
-console.log(environment.production ? 'Production' : 'Development');
 
 @NgModule({
-  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    ComponentsModule,
-    ServicesModule,
-    SharedModule,
   ],
-  providers: [
-    // AndroidFullScreen,
-    AppConfigService,
-    // SplashScreen,
-    // AppVersion,
-    // StatusBar,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  ],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
